a25f920f6b5657b2423791feec3c149c279becc7
Acer Nitro WSL <mac2x.manos@gmail.com>
2025-04-28 22:29:34 +0800

    Added restock procedure and improved error handling

--- Functions.sql
+++ Functions.sql
@@ -9,6 +9,12 @@ BEGIN
     SELECT @retVal = products.[productID] FROM products
     WHERE productName = @productName
 
+    IF @@RowCount > 1
+    BEGIN
+        RAISERROR('Duplicate entries found!', 3, 2)
+        RETURN NULL
+    END
+
     RETURN @retVal
 END;
 
@@ -20,6 +26,12 @@ BEGIN
     DECLARE @retVal BIGINT
     SELECT @retVal = kiosks.[kioskID] FROM kiosks
     WHERE username = @username
+
+    IF @@RowCount > 1
+    BEGIN
+        RAISERROR('Duplicate entries found!', 3, 2)
+        RETURN NULL
+    END
 
     RETURN @retVal
 END;
--- Procedures.sql
+++ Procedures.sql
@@ -33,7 +33,7 @@ CREATE PROCEDURE [purchaseProduct]
 	@amount INT
 AS
 BEGIN
-	DECLARE @salesID, @kioskID, @invID BIGINT
+	DECLARE @salesID BIGINT, @kioskID BIGINT, @invID BIGINT
 	SELECT @invID = [inventory ID]
 	FROM view_invProducts
 	WHERE [Product Name] = @productName
@@ -44,25 +44,79 @@ BEGIN
 		RETURN
 	END
 
-	INSERT INTO sales (amount, [date])
-	VALUES
-	(@amount, GETDATE())
+	BEGIN TRANSACTION purchase_product
+		INSERT INTO sales (amount, [date])
+		VALUES
+		(@amount, GETDATE())
 
-	@salesID = CAST(IDENT_CURRENT('sales') AS BIGINT)
-	@kioskID = getKioskID(@currUser)
+		@salesID = CAST(IDENT_CURRENT('sales') AS BIGINT)
+		@kioskID = getKioskID(@currUser)
 
-	IF @kioskID IS NULL
+		IF @kioskID IS NULL
+		BEGIN
+			RAISERROR('Invalid username!', 10, 1)
+			ROLLBACK
+			RETURN
+		END
+
+		INSERT INTO salesDetails (inventoryID, salesID, kioskID)
+		VALUES
+		(@invID, @salesID, @kioskID)
+
+		UPDATE inventory
+		SET quantity -= @amount
+		WHERE inventoryID = @invID
+
+		IF @@RowCount > 1
+		BEGIN
+			RAISERROR('Multiple rows updated rolling back changes...' 15, 1)
+			ROLLBACK
+			RETURN
+		END
+	COMMIT
+END;
+
+CREATE PROCEDURE [restockProduct]
+	@productName NVARCHAR(50),
+	@amount INT
+AS
+BEGIN
+	DECLARE @productID BIGINT, @invID BIGINT
+
+	SELECT @invID = [inventory ID]
+	FROM view_invProducts
+	WHERE [Product Name] = @productName
+
+	@productID = getProductID(@productName)
+
+	IF @productID IS NULL
 	BEGIN
-		RAISERROR('Invalid username!', 10, 1)
+		RAISERROR('Invalid product!', 10, 1)
 		RETURN
 	END
 
-	INSERT INTO salesDetails (inventoryID, salesID, kioskID)
-	VALUES
-	(@invID, @salesID, @kioskID)
+	IF @invID IS NULL
+	BEGIN
+		RAISERROR('Product not found in inventory!', 10, 1)
+		RETURN
+	END
 
-	UPDATE inventory
-	SET quantity -= @amount
-	WHERE inventoryID = @invID
+	BEGIN TRANSACTION restock_product
+		INSERT INTO restockHistory (inventoryID, amount, [dateTime])
+		VALUES
+		(@invID, @amount, GETDATE())
+
+		UPDATE inventory
+		SET quantity += @amount
+		WHERE productID = @productID
+
+		IF @@RowCount > 1
+		BEGIN
+			RAISERROR('Multiple rows affected rolling back changes...', 13, 1)
+			ROLLBACK
+			RETURN
+		END
+	COMMIT
 END;
+
 COMMIT;
